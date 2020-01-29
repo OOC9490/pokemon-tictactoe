1b4732b6046742983d0c6759c2ebe89ae47e5536
Lawrence Carreon <l.carreon123@gmail.com>
2020-01-29 15:47:15 +1100

    added end of game overlay, added click spam protection to the game and the user interface, improved UI styling and beginning work on a restart button

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@
 const game =
 {
     scoreCard: { 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0] },
-    cur: 0,
+    currentPlayer: 0, //0 for player 1, 1 for player 2
     teams: ["Valor","Mystic","Instinct","Rocket"],
     p0Team: "", //player 1
     p1Team: "", //player 2
     cpuEnabled: false,
     currentMoves: 0,
+    playerLabels: [],
 
     //Checks for a win or a draw
     check: function( id, player, team ){
@@ -23,21 +24,26 @@ const game =
             this.scoreCard[ winState ][ player ] += 1;  // register the current move by incrementing the counter for this win state
             if(this.scoreCard[ winState ][ player ] === 3 ){
             // if any of these reach 3, it means they've won
-            ui.endOfGameMsg(team, this.currentMoves);
+                ui.endOfGameMsg(team, this.currentMoves);
+                return;
             }
-        }
+        };
+        this.currentPlayer = 1 - player;  // switch between players (used as array index)
+        ui.playerTurnMsg(this.playerLabels[this.currentPlayer]);
     },
     setTeam: function( index, team ){
         this[team] = this.teams[index];
+        this.playerLabels.push(this[team]);
     }
 }
 $(document).ready(function(){
     $('.box').on('click', function() {
-        const currentPlayer = `p${game.cur}Team`;
-        $(this).addClass( 'p' + game.cur );  // play move into square
-        ui.drawSymbol( currentPlayer, $(this) );
-        game.currentMoves++;
-        game.check( $(this).attr('id'), game.cur, game[currentPlayer] );   // pass square ID into win check function
-        game.cur = 1 - game.cur;  // switch between players (used as array index)
+        if( $(this).hasClass('p0') === false && $(this).hasClass('p1') === false ){
+            const currentPlayer = `p${game.currentPlayer}Team`;
+            $(this).addClass( 'p' + game.currentPlayer );  // play move into square
+            ui.drawSymbol( currentPlayer, $(this) );
+            game.currentMoves++;
+            game.check( $(this).attr('id'), game.currentPlayer, game[currentPlayer] );   // pass square ID into win check function
+        };
     });
 });
--- js/ui.js
+++ js/ui.js
@@ -2,11 +2,14 @@ const ui = {
     symbolPaths: ["images/valor_logo.png","images/mystic_logo.png","images/instinct_logo.png","images/rocket_logo.png"],
     p0Team: "", //player 1
     p1Team: "", //player 2
+
     endOfGameMsg: function(turnPlayerTeam, moves){
+        $('#gameboard').addClass("disabled");
+        $('.endGameOverlay').fadeIn(1500);
         if(moves === 9){
-            $('<h1>').text("It is a draw!").appendTo(' body ');
+            $('#endGame').html(`It is a draw!`);
         }else{
-            $('<h1>').text( 'Team ' + turnPlayerTeam + ' Won' ).appendTo( 'body' );
+            $('#endGame').html( `Team  ${turnPlayerTeam} Won!<br>Play again with the same settings?` );
         };
     },
 
@@ -21,12 +24,33 @@ const ui = {
             $(this).css({"opacity":"1","transition":`${ 1.5 + increaseFade }s`});
             increaseFade += 0.5;
         });
+        $('#midgameRestart').css({"opacity":"1"});
+        setTimeout(function(){
+            $('#midgameRestart').removeClass('disabled');
+            $('.box').removeClass('disabled');
+            ui.playerTurnMsg(game.playerLabels[game.currentPlayer]);
+        },3000);
     },
 
     drawSymbol: function( turnPlayerTeam, $clickedBox ){
         $clickedBox.html(`<img src=${ui[turnPlayerTeam]}>`);
     },
+
+    playerTurnMsg: function(turnPlayerLabel){
+        $('#inGameMessages').fadeOut(250).html(`It is ${turnPlayerLabel}'s turn!`).fadeIn(250);
+    },
+
+    
 }
+
+$('#restart').on('click',function(){
+    ui.restart();
+});
+
+$('#midgameRestart').on('click',function(){
+    ui.restart();
+});
+
 $(document).ready(function(){
     $('.cpuChecker').on('click', function(){
         if($(this).value === "cpu"){
@@ -42,16 +66,16 @@ $(document).ready(function(){
         
         ui.setSymbol( $indexPosition, $team );
         if( ui.p0Team === ui.p1Team){
-            $('#userMessages').html("Player 2 can't have the same team as Player 1!").show();
-            $('#userMessages').delay(1500).fadeOut(1500);
+            $('#setupErrors').html("Player 2 can't have the same team as Player 1!").show().delay(1500).fadeOut(1500);
             return;
         };
-
-        $(`.${$team}`).fadeOut(1500);
+        $(`.${$team}`).addClass('disabled').fadeOut(1500);
         if( $team === 'p0Team'){
             $('.p1Team').delay(2100).fadeIn(1500);
         }else{
-            setTimeout(ui.showBoard,2100);
+            setTimeout(function(){
+                ui.showBoard();
+            },2100);
         };
     });
 });
